0e72533a6e8edabb7091af57aa0ff75af1cff2d6
Yuriy Tolstykh <yuriy.tolstykh@gmail.com>
2026-01-30 10:36:10 -0800

    adding host and hash

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
 import { createServer } from "http";
 import { Server } from "socket.io";
 import { socketHandler } from "./socketHandler";
+import * as child from "child_process";
 
-const PORT = 8080;
+const PORT = Number(process.env.PORT) || 8080;
+const HOST = "0.0.0.0";
+
+const commitHash = child
+  .execSync("git rev-parse --short HEAD")
+  .toString()
+  .trim();
+
+console.log("*** COMMIT HASH ***");
+console.log(`*** ${commitHash} ***`);
+console.log(`*** COMMIT HASH ***`);
 
 const origin =
   process.env.NODE_ENV === "production"
@@ -14,7 +25,11 @@ const origin =
       ]
     : ["http://localhost:5173"];
 
-const httpServer = createServer();
+const httpServer = createServer((_, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Please, use the API\n");
+});
 
 const io = new Server(httpServer, {
   path: "/battle-dice/",
@@ -31,4 +46,4 @@ console.log("------------ end cors ------------");
 
 socketHandler(io);
 
-httpServer.listen(PORT, "0.0.0.0");
+httpServer.listen(PORT, HOST);
