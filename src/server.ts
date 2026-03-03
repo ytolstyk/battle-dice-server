6fb83fde2e1e586c42256714039f486d33b8c43e
Yuriy Tolstykh <yuriy.tolstykh@gmail.com>
2026-03-03 15:01:01 -0800

    adding more cors

--- src/server.ts
+++ src/server.ts
@@ -22,6 +22,14 @@ const origin =
         "https://main.d2wj3ci2d6hgbv.amplifyapp.com/",
         "http://main.d2wj3ci2d6hgbv.amplifyapp.com:80",
         "https://main.d2wj3ci2d6hgbv.amplifyapp.com:433",
+        "https://dicebattles.com",
+        "https://dicebattles.com/",
+        "http://dicebattles.com:80",
+        "https://dicebattles.com:433",
+        "https://www.dicebattles.com",
+        "https://www.dicebattles.com/",
+        "http://www.dicebattles.com:80",
+        "https://www.dicebattles.com:433",
       ]
     : ["http://localhost:5173"];
 
@@ -40,9 +48,10 @@ const io = new Server(httpServer, {
   },
 });
 
+console.log("------------ START CORS ------------");
 console.log("Accepting origins:");
 origin.forEach((val) => console.log(val));
-console.log("------------ end cors ------------");
+console.log("------------ END CORS ------------");
 
 socketHandler(io);
 
